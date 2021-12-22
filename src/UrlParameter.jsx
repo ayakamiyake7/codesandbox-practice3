89b910d37e6fe1637e30021ae9418ae86fcd098e
ayakamiyake7 <88525743+ayakamiyake7@users.noreply.github.com>
2021-12-22 21:04:05 +0000

    Add parameter.

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,7 +1,16 @@
+import { useParams, useLocation } from "react-router-dom";
+
 export const UrlParameter = () => {
+  const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
+
   return (
     <div>
       <h1>This is URL Parameter.</h1>
+      <p>The Parameter is {id}.</p>
+      <p>The Queryparameter is {query.get("name")}.</p>
     </div>
   );
 };
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1Routes } from "./Page1Routes";
-import { Page2Routes } from "./Page2Routes";
+import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -13,7 +13,7 @@ export const Router = () => {
         path="/page1"
         render={({ match: { url } }) => (
           <Switch>
-            {Page1Routes.map((route) => (
+            {page1Routes.map((route) => (
               <Route
                 key={route.path}
                 exact={route.exact}
@@ -30,7 +30,7 @@ export const Router = () => {
         path="/page2"
         render={({ match: { url } }) => (
           <Switch>
-            {Page2Routes.map((route) => (
+            {page2Routes.map((route) => (
               <Route
                 key={route.path}
                 exact={route.exact}
